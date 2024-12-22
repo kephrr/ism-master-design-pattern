330dcfed148d87747d941db8c0564f036d66c2e5
Kephren NZE <kephrennze@gmail.com>
2024-12-22 11:04:12 +0000

    add: creer+afficher dep, creer vacataire, creer permanent, rattacher ens a dep

--- src/dao/DataBase.java
+++ src/dao/DataBase.java
@@ -11,15 +11,15 @@ public class DataBase {
     private ArrayList<Departement> departements = new ArrayList<>();
     private ArrayList<Enseignant> enseignants = new ArrayList<>();
 
-    public void addPermanent(Permanent permanent) {
+    public void ajouterPermanent(Permanent permanent) {
         enseignants.add(permanent);
     }
 
-    public void addVacataire(Vacataire vacataire) {
+    public void ajouterVacataire(Vacataire vacataire) {
         enseignants.add(vacataire);
     }
 
-    public void addDepartement(Departement departement) {
+    public void ajouterDepartement(Departement departement) {
         departements.add(departement);
     }
 
@@ -27,29 +27,33 @@ public class DataBase {
         return enseignants;
     }
 
-    public void showdAllDepartements() {
-        for (Departement departement : departements) System.out.println(departement);
+    public void afficherDepartements() {
+        for (Departement departement : departements) {
+            System.out.println("--------------------------------------------");
+            System.out.println(departement);
+        }
+        System.out.println("--------------------------------------------");
     }
 
-    public void showAllPermanents() {
+    public void afficherPermanents() {
         for (Enseignant enseignant : enseignants) if (enseignant instanceof Permanent) System.out.println(enseignant);
     }
 
-    public void showAllVacataires() {
+    public void afficherVacataires() {
         for (Enseignant enseignant : enseignants) if (enseignant instanceof Vacataire) System.out.println(enseignant);
     }
 
-    public Enseignant findEnseignantByMatricule(int matricule) {
+    public Enseignant rechercherEnseignantByMatricule(int matricule) {
         for (Enseignant enseignant : enseignants) if (enseignant.getMatricule() == matricule) return enseignant;
         return null;
     }
 
-    public Departement findDepartementByCode(String code) {
+    public Departement rechercherDepartementByCode(String code) {
         for (Departement departement : departements) if (departement.getCode().equals(code)) return departement;
         return null;
     }
 
-    public void bindDepartementToEnseignant(Enseignant e, Departement d) {
+    public void liaisonDepartementAEnseignant(Enseignant e, Departement d) {
         e.setAffectation(d);
         d.addEnseignants(e);
     }
--- src/presentation/Vue.java
+++ src/presentation/Vue.java
@@ -1,17 +1,21 @@
 package presentation;
 
+import dao.DataBase;
 import domain.Departement;
+import domain.Enseignant;
+import domain.Permanent;
+import domain.Vacataire;
 
 import java.util.Scanner;
 
 public class Vue {
     public static void main(String[] args) {
+        DataBase db = new DataBase();
         Scanner sc = new Scanner(System.in);
         int choix = 0;
         int matricule, nh, tauxh;
         String nom, prenom, grade, nomDep, codeDep;
-
-        String[] opts ={"Créer un Département", "Créer un Permanent","Créer un Vacataire",
+        String[] opts ={"Créer un Département","Afficher les départements", "Créer un Vacataire","Créer un Permanent",
                 "Rattacher un permanent à un département","Rattacher un vacataire à un département","Voire statistiques"};
         String[] resume ={"Continuer", "Quitter"};
         do{
@@ -23,15 +27,59 @@ public class Vue {
                     System.out.println("Entrez le code du département");
                     codeDep = sc.nextLine();
                     Departement d = new Departement(nomDep, codeDep);
+                    db.ajouterDepartement(d);
                     break;
                 case 2:
-                    System.out.println(opts[1]);
+                    db.afficherDepartements();
                     break;
                 case 3:
-                    System.out.println(opts[2]);
+                    System.out.println("Matricule :");
+                    matricule = sc.nextInt(); sc.nextLine();
+                    System.out.println("Nom du vacataire : ");
+                    nom = sc.nextLine();
+                    System.out.println("Prenom du vacataire :");
+                    prenom = sc.nextLine();
+                    System.out.println("Grade du vacataire :");
+                    grade = sc.nextLine();
+                    System.out.println("Nombre d'heures effectuées :");
+                    nh = sc.nextInt(); sc.nextLine();
+                    System.out.println("Taux horaire à payer :");
+                    tauxh = sc.nextInt(); sc.nextLine();
+                    Vacataire v = new Vacataire(matricule, nom, prenom, grade, nh, tauxh);
+                    db.ajouterVacataire(v);
                     break;
                 case 4:
-                    System.out.println(opts[3]);
+                    System.out.println("Matricule :");
+                    matricule = sc.nextInt(); sc.nextLine();
+                    System.out.println("Nom du vacataire : ");
+                    nom = sc.nextLine();
+                    System.out.println("Prenom du vacataire :");
+                    prenom = sc.nextLine();
+                    System.out.println("Grade du permanent :");
+                    grade = sc.nextLine();
+                    System.out.println("Salaire de base :");
+                    double salaire = sc.nextDouble();
+                    System.out.println("Prime :");
+                    double prime = sc.nextDouble();
+                    Permanent p = new Permanent(matricule, nom, prenom, grade, salaire, prime);
+                    db.ajouterPermanent(p);
+                    break;
+                case 5:
+                    System.out.println("Entrez le code du département concerné");
+                    codeDep = sc.nextLine();
+                    d = db.rechercherDepartementByCode(codeDep);
+                    if(d == null) {
+                        System.out.println("Erreur : departement inexistant");
+                        break;
+                    }
+                    System.out.println("Entrez le matricule de l'enseignant concerné");
+                    matricule = sc.nextInt(); sc.nextLine();
+                    Enseignant e = db.rechercherEnseignantByMatricule(matricule);
+                    if(e == null) {
+                        System.out.println("Erreur : enseignant inexistant");
+                        break;
+                    }
+                    db.liaisonDepartementAEnseignant(e, d);
                     break;
             }
         }while(select(sc, resume)!=2);
@@ -44,6 +92,7 @@ public class Vue {
                 System.out.println((i+1)+". "+options[i]);
             }
             choix = sc.nextInt();
+            sc.nextLine();
         }while(choix<1 || choix>options.length);
         return choix;
     }
